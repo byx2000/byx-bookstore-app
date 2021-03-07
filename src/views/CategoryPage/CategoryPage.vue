47742d83948a6d8cda4cd50c9e9c23df16455e40
byx2000 <416982860@qq.com>
2021-03-07 11:06:04 +0800

    完成电子书分类查询的分页展示

--- src/views/CategoryPage/CategoryPage.vue
+++ src/views/CategoryPage/CategoryPage.vue
@@ -1,8 +1,18 @@
 <template>
   <el-container>
     <el-main>
-      <category-choose :categories="categories" @optionChanged="optionChanged"/>
+      <category-choose :categories="categories" :selected="query" @optionChanged="optionChanged"/>
+      <h3>共 {{totalCount}} 条结果，共 {{totalPage}} 页，当前第 {{query.currentPage}} 页</h3>
       <book-grid :books="books"/>
+      <el-row type="flex" justify="center">
+        <el-pagination
+          background
+          layout="prev, pager, next"
+          :current-page="query.currentPage"
+          :page-count="totalPage"
+          @current-change="onCurrentPageChanged">
+        </el-pagination>
+      </el-row>
     </el-main>
   </el-container>
 </template>
@@ -20,34 +30,50 @@ export default {
   name: 'CategoryPage',
   data() {
     return {
+      query: {
+        categoryId: -1,
+        scoreRange: {},
+        heatRange: {},
+        updateAfter: 10000,
+        orderBy: 'score',
+        orderType: 'desc',
+        pageSize: 30,
+        currentPage: 1
+      },
       categories: [],
       books: [],
-      pageSize: 30,
-      currentPage: 1,
-      totalCount: 0
+      totalCount: 0,
+      totalPage: 0
     }
   },
   created() {
     getAllCategories().then(res => {
       this.categories = res.data
     })
-    getClassificationQueryBooks({
-      orderBy: 'score',
-      orderType: 'desc',
-      pageSize: this.pageSize,
-      currentPage: this.currentPage
-    }).then(res => {
+
+    getClassificationQueryBooks(this.query).then(res => {
       this.books = res.data.data
       this.totalCount = res.data.totalCount
+      this.totalPage = res.data.totalPage
     })
   },
   methods: {
     optionChanged(selected) {
-      selected.pageSize = this.pageSize
-      selected.currentPage = this.currentPage
-      getClassificationQueryBooks(selected).then(res => {
+      this.query = selected
+      this.query.pageSize = 30
+      this.query.currentPage = 1
+      getClassificationQueryBooks(this.query).then(res => {
         this.books = res.data.data
         this.totalCount = res.data.totalCount
+        this.totalPage = res.data.totalPage
+      })
+    },
+    onCurrentPageChanged(currentPage) {
+      this.query.currentPage = currentPage
+      getClassificationQueryBooks(this.query).then(res => {
+        this.books = res.data.data
+        this.totalCount = res.data.totalCount
+        this.totalPage = res.data.totalPage
       })
     }
   }
