5974fd063ad7d7d16982dc245ff7359cf31e29a1
byx2000 <416982860@qq.com>
2021-03-11 18:18:36 +0800

    完善电子书详情页

--- src/views/BookDetailPage/BookDetail.vue
+++ src/views/BookDetailPage/BookDetail.vue
@@ -1,18 +1,49 @@
 <template>
   <el-card class="box-card">
-    <div slot="header" class="clearfix">
-      <span class="book-name">{{book.name}}</span>
-    </div>
+    <el-row slot="header" type="flex" align="middle" justify="space-between">
+      <el-row type="flex">
+        <span class="book-name">{{book.name}}</span>
+        <el-row type="flex" class="book-score">
+          <el-rate
+            :value="bookScoreValue"
+            disabled>
+          </el-rate>
+          <span v-if="book.score !== undefined">{{book.score.toFixed(1)}}</span>
+        </el-row>
+        <span class="book-heat">
+          <i class="fa fa-fire" aria-hidden="true"></i> {{book.heat}}
+        </span>
+      </el-row>
+      <el-row type="flex">
+        <el-button type="danger" class="book-like-count">
+          <i class="fa fa-thumbs-up" aria-hidden="true"></i> {{book.likeCount}}
+        </el-button>
+        <el-button type="info" class="book-dislike-count">
+          <i class="fa fa-thumbs-down" aria-hidden="true"></i> {{book.dislikeCount}}
+        </el-button>
+      </el-row>
+    </el-row>
     <el-row type="flex">
       <el-col :span="3">
         <el-image class="book-cover" :src="book.cover"/>
       </el-col>
       <el-col :span="21">
         <el-row>
-          <span>作者：{{book.author}}</span>
-          <span>更新时间：{{book.updateTime}}</span>
-          <span>字数：1234</span>
+          <span class="book-author">
+            <i class="fa fa-user" aria-hidden="true"></i> {{book.author}}
+          </span>
+          <span class="book-update-time">
+            <i class="fa fa-clock-o" aria-hidden="true"></i> {{book.updateTime}}
+          </span>
+          <span class="book-word-count">字数：1234</span>
+          <el-button type="primary">
+            <i class="fa fa-book" aria-hidden="true"></i> 在线阅读
+          </el-button>
+          <el-button type="success">
+            <i class="fa fa-star-o" aria-hidden="true"></i> 收藏
+          </el-button>
         </el-row>
+        <el-divider></el-divider>
         <p class="book-description">简介：{{book.description}}</p>
       </el-col>
     </el-row>
@@ -29,13 +60,54 @@ export default {
         return {}
       }
     }
+  },
+  computed: {
+    bookScoreValue() {
+      return this.book.score / 2
+    }
   }
 }
 </script>
 
 <style scoped>
+.book-name {
+  font-weight: bold;
+  font-size: 18px;
+}
+
+.book-score {
+  font-size: 14px;
+  color: #ff9900;
+  margin-left: 20px;
+}
+
+.book-heat {
+  color: tomato;
+  font-size: 14px;
+  margin-left: 20px;
+}
+
 .book-cover {
   width: 156px;
   height: 208px;
+}
+
+.book-author {
+  font-size: 14px;
+}
+
+.book-update-time {
+  font-size: 14px;
+  margin-left: 20px;
+}
+
+.book-word-count {
+  font-size: 14px;
+  margin-left: 20px;
+  margin-right: 20px;
+}
+
+.book-description {
+  text-align:justify;
 }
 </style>
